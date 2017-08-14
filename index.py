ccf36b883e4ac5dea0fd3b75c2da10d1e79446ba
09apple <415807795@qq.com>
2017-08-14 23:43:25 +0800

    0814 md

--- index.py
+++ index.py
@@ -31,8 +31,8 @@ connect = pymysql.connect(
     host='localhost',
     port=3306,
     user='root',
-    passwd='09apple',
-    db='blog',
+    passwd='root',
+    db='bolg',
     charset='utf8'
 )
 
